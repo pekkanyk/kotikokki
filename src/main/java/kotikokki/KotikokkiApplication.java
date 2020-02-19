186e4483e1707fcc79f09efebf797b536f4144d2
Pekka Nykänen <pekka.nykanen@outlook.com>
2020-02-19 23:19:23 +0200

    Gradle tuki

--- src/main/java/kotikokki/KotikokkiApplication.java
+++ src/main/java/kotikokki/KotikokkiApplication.java
@@ -3,6 +3,11 @@ package kotikokki;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*import kotikokki.service.*;
+import kotikokki.controller.*;
+import kotikokki.domain.*;
+import kotikokki.repository.*;
+*/
 @SpringBootApplication
 public class KotikokkiApplication {
 
