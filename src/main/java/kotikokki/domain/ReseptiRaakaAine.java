ae684ceac577e1c64a023f2fd299021129a15d73
Pekka Nykänen <pekka.nykanen@outlook.com>
2020-03-07 21:45:37 +0200

    fix

--- src/main/java/kotikokki/domain/ReseptiRaakaAine.java
+++ src/main/java/kotikokki/domain/ReseptiRaakaAine.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
@@ -20,6 +21,7 @@ import org.springframework.data.jpa.domain.AbstractPersistable;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@EqualsAndHashCode(callSuper=false)
 public class ReseptiRaakaAine extends AbstractPersistable<Long> {
     @ManyToOne
     private RaakaAine raakaAine;
